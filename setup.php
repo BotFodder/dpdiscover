0b9ab290e2b3ae246e3ee3c447564b89bdabbb6a
Cacti SVC <cactiuser@svc-mon.net.usf.edu>
2015-11-06 15:20:13 -0500

    provisional 1.47 updates

--- setup.php
+++ setup.php
@@ -122,7 +122,7 @@ function dpdiscover_check_upgrade () {
 function plugin_dpdiscover_version () {
 	return array(
 		'name'     => 'dpdiscover',
-		'version'  => '1.46',
+		'version'  => '1.47',
 		'longname' => 'DP Discover',
 		'author'   => 'Eric Stewart',
 		'homepage' => 'http://runningoffatthemouth.com/?p=1067',
